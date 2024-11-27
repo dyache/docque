ac7f66f2827952865067b6eaeb9c00666d1d1566
Nurayat Beltayev <F1zm0@localhost.localdomain>
2024-11-27 09:52:07 +0500

    init

--- src/auth/models.py
+++ src/auth/models.py
@@ -12,4 +12,5 @@ class User(Base):
     __tablename__ = "user"
     user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
     iin: Mapped[str] = mapped_column(String(12))
+    name: Mapped[str] = mapped_column(String(50))
     hashed_password: Mapped[str] = mapped_column(String(255))
