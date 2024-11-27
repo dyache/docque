1f20a640437d1fe93919bb7903f92cb9364498b5
Nurayat Beltayev <F1zm0@localhost.localdomain>
2024-11-27 10:46:13 +0500

    feat: remove sqlAlchemy

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
-from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from sqlalchemy import create_engine
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 engine = create_engine("url", echo = True)
 conn = engine.connect()
