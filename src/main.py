32d26bf439c2e9609bf31ec2cf5dce9bcf21e004
Nurayat Beltayev <F1zm0@localhost.localdomain>
2024-12-07 14:49:20 +0500

    fix: cors issue

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.queue.routes import queue_router
 from src.queue_history.routes import queue_history_router
@@ -8,6 +9,8 @@ from src.student.routes import student_router
 # TODO: protect all other routes with auth
 
 app = FastAPI(root_path="/api/v1", title="docque API", description="This is qocque queue API", version="1.0.0")
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credential=True, allow_methods=["*"], allow_headers=["*"])
 app.include_router(prefix="/queue", router=queue_router)
 app.include_router(prefix="/staff", router=staff_router)
 app.include_router(prefix="/history", router=queue_history_router)
