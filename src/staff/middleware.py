16c85c4c6fa542d765d074873e275952e90ccb6f
Nurayat Beltayev <F1zm0@localhost.localdomain>
2024-12-10 12:41:34 +0500

    fix: auth fix

--- src/staff/middleware.py
+++ src/staff/middleware.py
@@ -1,8 +1,9 @@
+import hmac
 import uuid
 from typing import Annotated
 
 import jwt
-from fastapi import Depends, HTTPException, status, Header
+from fastapi import Depends, HTTPException, status
 from jwt import InvalidTokenError
 
 from src.db import settings
@@ -12,18 +13,14 @@ from src.staff.service import oauth2_scheme
 
 
 async def auth_middleware(token: Annotated[str, Depends(oauth2_scheme)], staff_repo: StaffRepositoryDep,
-                          tg_api_key: Annotated[str | None, Header()] = None) -> StaffSchema:
-    print(token)
+                          ) -> StaffSchema:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    print(tg_api_key)
-    if tg_api_key == settings.tg_bot_api_key:
-        print("tg-key")
+    if hmac.compare_digest(token, settings.tg_bot_api_key):
         return StaffSchema(name="tg")
-    print("not api key")
     try:
         payload = jwt.decode(token, settings.jwt_secret_key, algorithms=[settings.jwt_algorithm])
         print(payload)
