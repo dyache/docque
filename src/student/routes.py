c88989a39304c4ed415ba6d37adf78c11c4706d5
Nurayat Beltayev <F1zm0@localhost.localdomain>
2024-12-10 13:58:32 +0500

    fix: trying to debug

--- src/student/routes.py
+++ src/student/routes.py
@@ -1,17 +1,18 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 
 from src.staff.middleware import auth_middleware
 from src.staff.schema import StaffSchema
-from src.student.schema import StudentSchema
 from src.student.service import StudentServiceDep
 
 student_router = APIRouter()
 
 
 @student_router.post("/")
-def create(student_serv: StudentServiceDep, student: StudentSchema,
-           curr_user: Annotated[StaffSchema, Depends(auth_middleware)]) -> str:
+async def create(student_serv: StudentServiceDep, request: Request,
+                 curr_user: Annotated[StaffSchema, Depends(auth_middleware)]) -> str:
     print("creating thang")
-    return student_serv.create(student)
+    body = await request.body()
+    print(body)
+    # return student_serv.create(student)
